81f6a699c0f1652b1037749e0e3f87f705e26edc
omer bental <ombental@gmail.com>
2024-10-07 18:20:06 +0300

    using ijson

--- converter.py
+++ converter.py
@@ -1,3 +1,10 @@
+import ijson
+
+
 def convert():
     # TODO: read the data.json file and convert it to a simple readable table
-    pass
+    with open("data.json", "r") as file:
+        # meta, data
+        objects = ijson.items(file, "data.item")
+        for obj in objects:
+            print(obj)
