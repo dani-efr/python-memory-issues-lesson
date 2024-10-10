b1028fb2f153609f24770c09758c89d5e82ed9ea
dani-efr <69506413+dani-efr@users.noreply.github.com>
2024-10-10 11:21:02 +0300

    basic

--- converter.py
+++ converter.py
@@ -1,3 +1,22 @@
+import csv
+import json
+
+
 def convert():
-    # TODO: read the data.json file and convert it to a simple readable table
-    pass
+    # read the raw_data.json file and convert it to a simple readable table
+    headers = ["Row Number", "State", "Measure", "Value"]
+
+    with open("data/data.json", "r") as f:
+        raw_data = json.load(f)
+
+    with open("data/result.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        for i, line in enumerate(raw_data.get("data")):
+            row_number = i+1
+            state = line[9]
+            measure = line[13]
+            value = line[14]
+            row = [row_number, state, measure, value]
+            writer.writerow(row)
+
--- highscore.py
+++ highscore.py
@@ -1,3 +1,20 @@
+import csv
+
+
 def highscore():
-    # TODO
-    pass
+    measure = "Pedestrians"
+
+    with open("data/result.csv", "r", newline="") as f:
+        reader = csv.reader(f, delimiter=",")
+        filtered_results = [line for line in reader if line[2] == measure]
+        filtered_results.sort(key=lambda x: x[3], reverse=True)
+        top_five = filtered_results[:5]
+
+    with open("data/top_5.csv", "w", newline="") as f:
+        writer = csv.writer(f, delimiter=",")
+        writer.writerows(top_five)
+
+
+if __name__ == "__main__":
+    highscore()
+
